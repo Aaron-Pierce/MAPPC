b6596861c823712afc93695c46533a15f69ebac4
SAXTEN2011 <amaxpierce@yahoo.com>
2018-11-23 11:25:01 -0600

    reworked leaderboard population and implemented open leaderboard

--- js/index.js
+++ js/index.js
@@ -1,3 +1,46 @@
+Array.prototype.pushScoreSort = function (e, level, type) {
+    if (e[level + "Correct"] === 0 && e[level + "Wrong"] === 0) {
+        return; //no need to add players who have never attempted a leaderboard.
+    }
+    if (this.length === 0) {
+        this.push(e)
+        return;
+    }
+    for (let i = 0; i < this.length; i++) {
+        if (e[level + "Correct"] > this[i][level + "Correct"]) {
+            this.splice(i, 0, e);
+            return;
+        }
+    }
+    this.push(e); //if the length isnt zero, and its gone through the length comparison, this is the smallest element in the array, so just push it
+}
+
+function getScore(player) {
+    return player.aCorrect + (player.bCorrect * 2) + (player.cCorrect * 3);
+}
+
+Array.prototype.pushScoreSortOpen = function (e) {
+
+    if (this.length === 0) {
+        this.push(e)
+        return;
+    }
+    let score = getScore(e);
+    for (let i = 0; i < this.length; i++) {
+        if (score > getScore(this[i])) {
+            this.splice(i, 0, e);
+            return;
+        }
+    }
+    this.push(e)
+}
+
+String.prototype.firstLetterUpperCase = function () {
+    let split = this.split("");
+    split[0] = split[0].toUpperCase();
+    return split.join("")
+}
+
 var instance = M.Tabs.init(document.querySelectorAll(".tabs"), {});
 
 
@@ -10,7 +53,9 @@ let cLegacy = document.getElementById("cLegacy");
 let open = document.getElementById("open");
 
 let db = firebase.firestore();
-const settings = {timestampsInSnapshots: true}
+const settings = {
+    timestampsInSnapshots: true
+}
 db.settings(settings)
 
 // let aRef = db.collection("a");
@@ -20,109 +65,76 @@ db.settings(settings)
 let players = db.collection("players");
 
 
-(players.where("level", "==", "a")).orderBy("aCorrect", "desc").limit(20).get().then(function(data){
-    let aLive = document.getElementById("aLiveTableData");
-    let rank = 1;
-    for(doc of data.docs){
-        let d = doc.data();
-        aLive.innerHTML += `<tr>
-        <td>${rank}</td>
-        <td>${d.firstName} ${d.lastName}</td>
-        <td>${d.aCorrect}</td>
-        <td>${d.aWrong}</td>
-        <td>${d.aCorrect + d.aWrong}</td>
-        <td>${(d.aCorrect / (d.aCorrect + d.aWrong)).toFixed(2) * 100}%</td>
-        </tr>`
-        rank++;
+let leaderboard = {
+    a: {
+        live: [],
+        legacy: []
+    },
+    b: {
+        live: [],
+        legacy: []
+    },
+    c: {
+        live: [],
+        legacy: []
+    },
+    open: {
+        legacy: []
     }
+}
+
+players.get().then(function (data) {
+    for (doc of data.docs) {
+        let level = doc.data().level;
+        leaderboard[level].live.pushScoreSort(doc.data(), level)
+        leaderboard.a.legacy.pushScoreSort(doc.data(), 'a');
+        leaderboard.b.legacy.pushScoreSort(doc.data(), 'b');
+        leaderboard.c.legacy.pushScoreSort(doc.data(), 'c');
+        leaderboard.open.legacy.pushScoreSortOpen(doc.data())
+    }
+
+    for (board in leaderboard) {
+        for (bracket in leaderboard[board]) {
+            let rank = 1;
+            for (player of leaderboard[board][bracket]) {
+                if (board !== "open") {
+                    // console.log(player);
+                    console.log(board + bracket.firstLetterUpperCase() + "TableData")
+                    document.getElementById(board + bracket.firstLetterUpperCase() + "TableData").innerHTML += `<tr>
+                        <td>${rank}</td>
+                        <td>${player.firstName} ${player.lastName}</td>
+                        <td>${player[board + "Correct"]}</td>
+                        <td>${player[board + "Wrong"]}</td>
+                        <td>${player[board + "Correct"] + player[board + "Wrong"]}</td>
+                        <td>${(player[board + "Correct"] / (player[board + "Correct"] + player[board + "Wrong"])).toFixed(2) * 100 || 100}%</td>
+                    </tr>`
+                } else if (board === "open") {
+                    document.getElementById(board + bracket.firstLetterUpperCase() + "TableData").innerHTML += `<tr>
+                    <td>${rank}</td>
+                    <td>${player.firstName} ${player.lastName}</td>
+                    <td>${getScore(player)}</td>
+                    <td>${player.aCorrect + player.bCorrect + player.cCorrect}</td>
+                    <td>${player.aCorrect + player.aWrong + player.bCorrect + player.bWrong + player.cCorrect + player.cWrong }</td>
+                    <td>${((player.aCorrect + player.bCorrect + player.cCorrect) / (player.aCorrect + player.aWrong + player.bCorrect + player.bWrong + player.cCorrect + player.cWrong )).toFixed(2) * 100}%</td>
+                    <td>${player.aCorrect}</td>
+                    <td>${player.aWrong}</td>
+                    <td>${(player.aCorrect / (player.aCorrect + player.aWrong)).toFixed(2) * 100 || 100}%</td>
+                    <td>${player.bCorrect}</td>
+                    <td>${player.bWrong}</td>
+                    <td>${(player.bCorrect / (player.bCorrect + player.bWrong)).toFixed(2) * 100 || 100}%</td>
+                    <td>${player.cCorrect}</td>
+                    <td>${player.cWrong}</td>
+                    <td>${(player.cCorrect / (player.cCorrect + player.cWrong)).toFixed(2) * 100 || 100}%</td>
+                    </tr>`
+                }
+                rank++;
+            }
+        }
+    }
+
 })
 
 
-players.where("level", "==", "b").orderBy("bCorrect", "desc").limit(20).get().then(function(data){
-    let aLive = document.getElementById("bLiveTableData");
-    let rank = 1;
-    for(doc of data.docs){
-        let d = doc.data();
-        aLive.innerHTML += `<tr>
-        <td>${rank}</td>
-        <td>${d.firstName} ${d.lastName}</td>
-        <td>${d.bCorrect}</td>
-        <td>${d.bWrong}</td>
-        <td>${d.bCorrect + d.bWrong}</td>
-        <td>${(d.bCorrect / (d.bCorrect + d.bWrong)).toFixed(2) * 100}%</td>
-        </tr>`
-        rank++;
-    }
-})
-
-players.where("level", "==", "c").orderBy("cCorrect", "desc").limit(20).get().then(function(data){
-    let aLive = document.getElementById("cLiveTableData");
-    let rank = 1;
-    for(doc of data.docs){
-        let d = doc.data();
-        aLive.innerHTML += `<tr>
-        <td>${rank}</td>
-        <td>${d.firstName} ${d.lastName}</td>
-        <td>${d.cCorrect}</td>
-        <td>${d.cWrong}</td>
-        <td>${d.cCorrect + d.cWrong}</td>
-        <td>${(d.cCorrect / (d.cCorrect + d.cWrong)).toFixed(2) * 100}%</td>
-        </tr>`
-        rank++;
-    }
-})
 
 
-
-players.orderBy("aCorrect", "desc").limit(20).get().then(function(data){
-    let aLive = document.getElementById("aLegacyTableData");
-    let rank = 1;
-    for(doc of data.docs){
-        let d = doc.data();
-        aLive.innerHTML += `<tr>
-        <td>${rank}</td>
-        <td>${d.firstName} ${d.lastName}</td>
-        <td>${d.aCorrect}</td>
-        <td>${d.aWrong}</td>
-        <td>${d.aCorrect + d.aWrong}</td>
-        <td>${(d.aCorrect / (d.aCorrect + d.aWrong)).toFixed(2) * 100}%</td>
-        </tr>`
-        rank++;
-    }
-})
-
-
-players.orderBy("bCorrect", "desc").limit(20).get().then(function(data){
-    let aLive = document.getElementById("bLegacyTableData");
-    let rank = 1;
-    for(doc of data.docs){
-        let d = doc.data();
-        aLive.innerHTML += `<tr>
-        <td>${rank}</td>
-        <td>${d.firstName} ${d.lastName}</td>
-        <td>${d.bCorrect}</td>
-        <td>${d.bWrong}</td>
-        <td>${d.bCorrect + d.bWrong}</td>
-        <td>${(d.bCorrect / (d.bCorrect + d.bWrong)).toFixed(2) * 100}%</td>
-        </tr>`
-        rank++;
-    }
-})
-
-players.orderBy("cCorrect", "desc").limit(20).get().then(function(data){
-    let aLive = document.getElementById("cLegacyTableData");
-    let rank = 1;
-    for(doc of data.docs){
-        let d = doc.data();
-        aLive.innerHTML += `<tr>
-        <td>${rank}</td>
-        <td>${d.firstName} ${d.lastName}</td>
-        <td>${d.cCorrect}</td>
-        <td>${d.cWrong}</td>
-        <td>${d.cCorrect + d.cWrong}</td>
-        <td>${(d.cCorrect / (d.cCorrect + d.cWrong)).toFixed(2) * 100}%</td>
-        </tr>`
-        rank++;
-    }
-})
-
+/*        */
